70741f5d03fad9efbdc64747764703623de3b3f9
mkovarsky <mikhail.kovarsky@gmail.com>
2020-06-17 15:05:57 +0300

    split tests

---
+++ src/test/java/test/HappyPathTest.java
@@ -0,0 +1,88 @@
+package test;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import data.DataHelper;
+import io.qameta.allure.selenide.AllureSelenide;
+import lombok.val;
+import org.junit.jupiter.api.*;
+import page.TourOfferPage;
+import sqlUtils.SqlUtils;
+
+import java.sql.SQLException;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class HappyPathTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:8080");
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @DisplayName("Позитивный сценарий. Успешная покупка по карте.")
+    @Test
+    public void shouldConfirmPaymentWithApprovedCard() throws SQLException {
+        val tourOfferPage = new TourOfferPage();
+        val paymentPage = tourOfferPage.payByCard();
+        val approvedCardInformation = DataHelper.getValidCardInformation();
+        paymentPage.enterCardData(approvedCardInformation);
+        paymentPage.successfulPayment();
+        val paymentId = SqlUtils.getPaymentId();
+        val statusForPayment = SqlUtils.getStatusForPayment(paymentId);
+        Assertions.assertEquals("APPROVED", statusForPayment);
+    }
+
+    @DisplayName("Позитивный сценарий. Успешная покупка в кредит.")
+    @Test
+    public void shouldConfirmBuyingOnCreditWithApprovedCard() throws SQLException {
+        val tourOfferPage = new TourOfferPage();
+        val paymentPage = tourOfferPage.buyOnCredit();
+        val approvedCardInformation = DataHelper.getValidCardInformation();
+        paymentPage.enterCardData(approvedCardInformation);
+        paymentPage.successfulPayment();
+        val paymentId = SqlUtils.getPaymentId();
+        val statusForCredit = SqlUtils.getStatusForCredit(paymentId);
+        assertEquals("APPROVED", statusForCredit);
+    }
+
+    @DisplayName("Позитивный сценарий. Покупка по отклоняемой карте.")
+    @Test
+    public void shouldNotConfirmPaymentWithDeclinedCard() throws SQLException {
+        val tourOfferPage = new TourOfferPage();
+        val paymentPage = tourOfferPage.payByCard();
+        val declinedCardInformation = DataHelper.getInvalidCardInformation();
+        paymentPage.enterCardData(declinedCardInformation);
+        paymentPage.notSuccessfulPayment();
+        val paymentId = SqlUtils.getPaymentId();
+        val statusForPayment = SqlUtils.getStatusForPayment(paymentId);
+        assertThat(statusForPayment, equalTo("DECLINED"));
+    }
+
+    @DisplayName("Позитивный сценарий. Покупка в кредит по данным отклоняемой карты.")
+    @Test
+    public void shouldNotConfirmBuyingOnCreditWithDeclinedCard() throws SQLException {
+        val tourOfferPage = new TourOfferPage();
+        val paymentPage = tourOfferPage.buyOnCredit();
+        val declinedCardInformation = DataHelper.getInvalidCardInformation();
+        paymentPage.enterCardData(declinedCardInformation);
+        paymentPage.notSuccessfulPayment();
+        val paymentId = SqlUtils.getPaymentId();
+        val statusFortCredit = SqlUtils.getStatusForCredit(paymentId);
+        assertThat(statusFortCredit, equalTo("DECLINED"));
+    }
+
+}
--- src/test/java/test/SadPathTest.java
+++ src/test/java/test/SadPathTest.java
@@ -6,17 +6,11 @@ import io.qameta.allure.selenide.AllureSelenide;
 import lombok.val;
 import org.junit.jupiter.api.*;
 import page.TourOfferPage;
-import sqlUtils.SqlUtils;
-
-import java.sql.SQLException;
 
 import static com.codeborne.selenide.Selenide.open;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-public class UITest {
+public class SadPathTest {
 
     @BeforeAll
     static void setUpAll() {
@@ -33,59 +27,9 @@ public class UITest {
         SelenideLogger.removeListener("allure");
     }
 
-    @DisplayName("Позитивный сценарий. Успешная покупка по карте.")
-    @Test
-    public void shouldConfirmPaymentWithApprovedCard() throws SQLException {
-        val tourOfferPage = new TourOfferPage();
-        val paymentPage = tourOfferPage.payByCard();
-        val approvedCardInformation = DataHelper.getValidCardInformation();
-        paymentPage.enterCardData(approvedCardInformation);
-        paymentPage.successfulPayment();
-        val paymentId = SqlUtils.getPaymentId();
-        val statusForPayment = SqlUtils.getStatusForPayment(paymentId);
-        Assertions.assertEquals("APPROVED", statusForPayment);
-    }
-
-    @DisplayName("Позитивный сценарий. Успешная покупка в кредит.")
-    @Test
-    public void shouldConfirmBuyingOnCreditWithApprovedCard() throws SQLException {
-        val tourOfferPage = new TourOfferPage();
-        val paymentPage = tourOfferPage.buyOnCredit();
-        val approvedCardInformation = DataHelper.getValidCardInformation();
-        paymentPage.enterCardData(approvedCardInformation);
-        paymentPage.successfulPayment();
-        val paymentId = SqlUtils.getPaymentId();
-        val statusForCredit = SqlUtils.getStatusForCredit(paymentId);
-        assertEquals("APPROVED", statusForCredit);
-    }
-
-    @DisplayName("Позитивный сценарий. Покупка по отклоняемой карте.")
-    @Test
-    public void shouldNotConfirmPaymentWithDeclinedCard() throws SQLException {
-        val tourOfferPage = new TourOfferPage();
-        val paymentPage = tourOfferPage.payByCard();
-        val declinedCardInformation = DataHelper.getInvalidCardInformation();
-        paymentPage.enterCardData(declinedCardInformation);
-        paymentPage.notSuccessfulPayment();
-        val paymentId = SqlUtils.getPaymentId();
-        val statusForPayment = SqlUtils.getStatusForPayment(paymentId);
-        assertThat(statusForPayment, equalTo("DECLINED"));
-    }
-    @DisplayName("Позитивный сценарий. Покупка в кредит по данным отклоняемой карты.")
-    @Test
-    public void shouldNotConfirmBuyingOnCreditWithDeclinedCard() throws SQLException {
-        val tourOfferPage = new TourOfferPage();
-        val paymentPage = tourOfferPage.buyOnCredit();
-        val declinedCardInformation = DataHelper.getInvalidCardInformation();
-        paymentPage.enterCardData(declinedCardInformation);
-        paymentPage.notSuccessfulPayment();
-        val paymentId = SqlUtils.getPaymentId();
-        val statusFortCredit = SqlUtils.getStatusForCredit(paymentId);
-        assertThat(statusFortCredit, equalTo("DECLINED"));
-    }
     @DisplayName("Негативный сценарий. Покупка с незаполненными данными карты.")
     @Test
-    public void shouldNotConfirmPaymentWithEmptyForm(){
+    public void shouldNotConfirmPaymentWithEmptyForm() {
         val tourOfferPage = new TourOfferPage();
         val paymentPage = tourOfferPage.payByCard();
         val invalidCardInformation = DataHelper.getEmptyCardInformation();
@@ -95,13 +39,14 @@ public class UITest {
 
     @DisplayName("Негативный сценарий. Покупка в кредит с незаполненными данными карты.")
     @Test
-    public void shouldNotConfirmBuyingOnCreditWithEmptyForm(){
+    public void shouldNotConfirmBuyingOnCreditWithEmptyForm() {
         val tourOfferPage = new TourOfferPage();
         val paymentPage = tourOfferPage.buyOnCredit();
         val invalidCardInformation = DataHelper.getEmptyCardInformation();
         paymentPage.enterCardData(invalidCardInformation);
         paymentPage.invalidFormat();
     }
+
     @DisplayName("Негативный сценарий. Покупка по карте с невалидным номером.")
     @Test
     public void shouldNotConfirmPaymentWithInvalidCardNumber() {
@@ -111,6 +56,7 @@ public class UITest {
         paymentPage.enterCardData(invalidCardInformation);
         paymentPage.notSuccessfulPayment();
     }
+
     @DisplayName("Негативный сценарий. Покупка в кредит по данным карты с невалидным номером.")
     @Test
     public void shouldNotConfirmBuyingOnCreditWithInvalidCardNumber() {
@@ -130,6 +76,7 @@ public class UITest {
         paymentPage.enterCardData(invalidCardInformation);
         paymentPage.expiredYear();
     }
+
     @DisplayName("Негативный сценарий. Покупка в кредит по по данным карты с истекшим сроком действия(в прошлых годах).")
     @Test
     public void shouldNotConfirmBuyingOnCreditWithExpiredYearCard() {
@@ -139,6 +86,7 @@ public class UITest {
         paymentPage.enterCardData(invalidCardInformation);
         paymentPage.expiredYear();
     }
+
     @DisplayName("Негативный сценарий. Покупка по карте с истекшим сроком действия(в прошлых месяце).")
     @Test
     public void shouldNotConfirmPaymentWithExpiredMonthCard() {
@@ -148,6 +96,7 @@ public class UITest {
         paymentPage.enterCardData(invalidCardInformation);
         paymentPage.expiredMonth();
     }
+
     @DisplayName("Негативный сценарий. Покупка в кредит по по данным карты с истекшим сроком действия(в прошлых месяце).")
     @Test
     public void shouldNotConfirmBuyingOnCreditWithExpiredMonthCard() {
@@ -157,6 +106,7 @@ public class UITest {
         paymentPage.enterCardData(invalidCardInformation);
         paymentPage.expiredMonth();
     }
+
     @DisplayName("Негативный сценарий. Покупка по карте с неверным форматом полей.")
     @Test
     public void shouldNotConfirmPaymentWithWrongFormatFieldsCard() {
@@ -166,6 +116,7 @@ public class UITest {
         paymentPage.enterCardData(invalidCardInformation);
         paymentPage.invalidFormat();
     }
+
     @DisplayName("Негативный сценарий. Покупка в кредит по по данным карты с неверным форматом полей.")
     @Test
     public void shouldNotConfirmBuyingOnCreditWithWrongFormatFieldsCard() {
